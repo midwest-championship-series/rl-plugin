39b01f146e1f4fd9a7401db258d0aea7e7b4a9b4
Benjamin Nylund <31191383+bnylund@users.noreply.github.com>
2021-05-10 11:12:07 -0500

    Migrated to SOS v1.6

---
+++ SOS/Classes/WebsocketManager.cpp
@@ -0,0 +1,98 @@
+#include "WebsocketManager.h"
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "json.hpp"
+#include "utils/parser.h"
+
+using nlohmann::json;
+
+// PUBLIC FUNCTIONS //
+WebsocketManager::WebsocketManager(std::shared_ptr<CVarManagerWrapper> cvarManager, std::shared_ptr<GameWrapper> gameWrapper)
+    : cvarManager(cvarManager), gameWrapper(gameWrapper) {
+
+    cvarServer = std::make_shared<std::string>("http://localhost:5555");
+    cvarToken = std::make_shared<std::string>("wTJ2bAQW9R6M89NTSaqUsAvQ");
+
+    CVarWrapper server = cvarManager->registerCvar("sos_server", "http://localhost:5555", "Server to send events to", true);
+    server.bindTo(cvarServer);
+    server.addOnValueChanged([cvarManager, gameWrapper, this](std::string s, CVarWrapper c) {
+        // Stop websocket client then connect to new server
+        if (h.opened()) {
+            cvarManager->log("[SOS-SocketIO] Closing previous connection");
+            h.sync_close();
+        }
+        cvarManager->log("[SOS-SocketIO] new server: " + c.getStringValue());
+        h.clear_con_listeners();
+        h.clear_socket_listeners();
+        h.set_reconnect_delay(0);
+        h.set_reconnect_delay_max(1000);
+        h.set_reconnect_attempts(0);
+        h.set_reconnecting_listener([cvarManager, &c]() {
+            cvarManager->log("[SOS-SocketIO] Attempting to reconnect to '" + c.getStringValue() + "'...");
+        });
+        h.set_fail_listener([cvarManager]() {
+            cvarManager->log("[SOS-SocketIO] Failed to connect.");
+        });
+        h.set_open_listener([gameWrapper, cvarManager, this]() {
+            gameWrapper->SetTimeout([gameWrapper, cvarManager, this](GameWrapper*) {
+                cvarManager->log("[SOS-SocketIO] emitting register");
+                h.socket()->emit("mncs_register");
+                cvarManager->log("[SOS-SocketIO] Connected! Attempting to log in...");
+                h.socket()->emit("login", cvarManager->getCvar("sos_token").getStringValue(), [cvarManager](const sio::message::list& list) {
+                    cvarManager->log("[SOS-SocketIO] Got callback");
+                    if (list[0]->get_string() == "good")
+                        cvarManager->log("[SOS-SocketIO] Login successful!");
+                    else
+                        cvarManager->log("[SOS-SocketIO] Login failed!");
+                });
+            }, .3F);
+        });
+        cvarManager->log("[SOS-SocketIO] Attempting to connect to " + c.getStringValue());
+        h.connect(c.getStringValue());
+    });
+
+    cvarManager->registerCvar("sos_token", "wTJ2bAQW9R6M89NTSaqUsAvQ", "Token for authentication").bindTo(cvarToken);
+}
+
+void WebsocketManager::SendEvent(std::string eventName, const json &jsawn)
+{
+    json event;
+    event["event"] = eventName;
+    event["data"] = jsawn;
+
+    if (h.opened()) {
+        h.socket()->emit("game event", event.dump());
+    }
+}
+
+void WebsocketManager::StartClient()
+{
+    cvarManager->log("[SOS-SocketIO] Starting Client (" + *cvarServer + ")");
+    h.set_reconnect_delay(0);
+    h.set_reconnect_delay_max(1000);
+    h.set_reconnect_attempts(3);
+    h.set_reconnecting_listener([&]() {
+        cvarManager->log("[SOS-SocketIO] Attempting to reconnect to '" + *cvarServer + "'...");
+    });
+    h.set_fail_listener([&]() {
+        cvarManager->log("[SOS-SocketIO] Failed to connect to '" + *cvarServer + "'.");
+    });
+    h.set_open_listener([&]() {
+        gameWrapper->SetTimeout([&](GameWrapper*) {
+            cvarManager->log("[SOS-SocketIO] emitting register");
+            h.socket()->emit("mncs_register");
+            cvarManager->log("[SOS-SocketIO] Connected to " + *cvarServer + "! Attempting to log in...");
+            h.socket()->emit("login", *cvarToken, [&](const sio::message::list& list) {
+                cvarManager->log("[SOS-SocketIO] Got callback");
+                if (list[0]->get_string() == "good")
+                    cvarManager->log("[SOS-SocketIO] Login successful!");
+                else
+                    cvarManager->log("[SOS-SocketIO] Login failed!");
+            });
+        }, .3F);
+    });
+    h.connect(*cvarServer);
+}
+
+void WebsocketManager::StopClient() {
+    h.close();
+}
---
+++ SOS/Classes/WebsocketManager.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <set>
+#include "Plugin/MacrosStructsEnums.h"
+#include "json.hpp"
+#include "Plugin/SOS.h"
+#include "socketio/sio_client.h"
+
+
+using nlohmann::json;
+class CVarManagerWrapper;
+class SOS;
+
+//#define USE_TLS
+
+#ifdef USE_TLS
+#pragma comment(lib, "sioclient_tls.lib")
+#pragma comment(lib, "crypt32.lib")
+#pragma comment(lib, "libcrypto64MT.lib")
+#pragma comment(lib, "libssl64MT.lib")
+#else
+#pragma comment(lib, "sioclient.lib")
+#endif
+
+class WebsocketManager
+{
+public:
+    WebsocketManager(std::shared_ptr<CVarManagerWrapper> cvarManager, std::shared_ptr<GameWrapper> gameWrapper);
+
+    void StartClient();
+    void StopClient();
+    
+    void SendEvent(std::string eventName, const json& jsawn);
+
+private:
+    WebsocketManager() = delete; // No default constructor
+
+    std::shared_ptr<std::string> cvarServer;
+    std::shared_ptr<std::string> cvarToken;
+    std::shared_ptr<CVarManagerWrapper> cvarManager;
+    std::shared_ptr<GameWrapper> gameWrapper;
+    sio::client h;
+};
